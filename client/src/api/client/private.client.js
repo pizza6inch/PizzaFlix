3d4cb0acf0ca130346d2e4ffbb9b67b155f85368
huiwuzhang <huiwuzhang@eztravel.com.tw>
2024-09-09 15:57:04 +0800

    baseURL

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
+import { baseURL } from '../configs/server.configs'
 
-const baseURL = 'http://127.0.0.1:5000/api/v1'
+// const baseURL = 'http://127.0.0.1:5000/api/v1'
 
 const privateClient = axios.create({
   baseURL,
