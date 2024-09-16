f685801c7a2e3dccb49b2c0f24e428ecdb42cb8a
huiwuzhang <huiwuzhang@eztravel.com.tw>
2024-09-16 10:35:17 +0800

    change Logo

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -3,15 +3,16 @@ import { Typography } from '@mui/material'
 import { useTheme } from '@emotion/react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { themeModes } from '../../configs/theme.configs.js'
 
 const Logo = () => {
   const theme = useTheme()
-  const { mode } = useSelector(state => state.theme)
+  const { themeMode } = useSelector(state => state.themeMode)
 
   return (
     <Link to="/" style={{ textDecoration: 'none' }}>
       <Typography fontWeight={'700'} fontSize="1.7rem">
-        <span style={mode === 'dark' ? { color: 'white' } : { color: 'black' }}>Pizza</span>
+        <span style={themeMode === themeModes.dark ? { color: 'white' } : { color: 'black' }}>Pizza</span>
         <span style={{ color: theme.palette.primary.main }}>Flix</span>
       </Typography>
     </Link>
